d49789d09fee5a5a1e622433362a1e23e9eacbb6
ramrk399 <ramavtarchauhan2000@gmail.com>
2024-08-21 11:17:02 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -40,76 +40,61 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🅢—————————"
+        bar = "✧▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—🅞————————"
+        bar = "▰✧▱▱▱▱▱▱▱▰"
     elif 20 <= umm < 30:
-        bar = "——🅝———————"
+        bar = "▰▱✧▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———🅐——————"
+        bar = "▰▱▱✧▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————♡—————"
+        bar = "▰▱▱▱✧▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "—————🅡————"
+        bar = "▰▱▱▱▱✧▱▱▱▰"
     elif 60 <= umm < 70:
-        bar = "——————🅐———"
+        bar = "▰▱▱▱▱▱✧▱▱▰"
     elif 70 <= umm < 80:
-        bar = "———————🅙——"
+        bar = "▰▱▱▱▱▱▱✧▱▰"
     elif 80 <= umm < 95:
-        bar = "————————🅐—"
+        bar = "▰▱▱▱▱▱▱▱✧▰"
     else:
-        bar = "—————————♡"
-
+        bar = "▰▱▱▱▱▱▱▱▰✧"
     buttons = [
-        [
-            InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🔇", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🔁", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="🛑", callback_data=f"ADMIN Stop|{chat_id}"),
-        ]
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-      [
-            InlineKeyboardButton(
-                text="𝐎𝐰𝐧𝐞𝐫 🥀", url="https://t.me/lll_ISTKHAR_ll",
-            ),
-            InlineKeyboardButton(
-                text="𝐂𝐡𝐚𝐭 🥀", url="https://t.me/MUNNA_INDO_IG_STORE",
-            )
+        [
+            InlineKeyboardButton(text="◁ ʀᴇꜱᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊ ᴘᴀᴜꜱᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷ ꜱᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-        ]
-
-    return buttons
-
-
-def stream_markup(_, videoid, chat_id):
-    buttons = [
         [
-            InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🔇", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🔁", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="🛑", callback_data=f"ADMIN Stop|{chat_id}"),
-        ]
-        [
-            InlineKeyboardButton(
-                text="𝐎𝐰𝐧𝐞𝐫🥀", url="https://t.me/lll_ISTKHAR_ll",
-            ),
-            InlineKeyboardButton(
-                text="𝐂𝐡𝐚𝐭🥀", url="https://t.me/MUNNA_INDO_IG_STORE",
-            )
+            InlineKeyboardButton(text="▢ ꜱᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ",url=f"",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-
     return buttons
 
+
+def stream_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(text="◁ ʀᴇꜱᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊ ᴘᴀᴜꜱᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷ ꜱᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(text="▢ ꜱᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ",url=f"https://t.me/Queen_update",),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
+    
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
